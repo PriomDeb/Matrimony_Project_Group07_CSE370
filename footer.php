6e2d6930ba626ea87bdb790fb71cf7f4e37fee43
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2022-08-21 09:50:15 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     	<div class="container">
     		<div class="col-md-4 col_2">
     			<h4>About Us</h4>
-    			<p>CSE370 Group 07</p>
+    			<p>Priom Deb from CSE370 Group 07</p>
     		</div>
             
     		<div class="col-md-2 col_2">
