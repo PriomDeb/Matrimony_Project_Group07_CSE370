53e6125284dda49c786ea9e69b10f346edd71a81
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2022-08-26 21:34:37 +0600

    Implemented report user

--- report.php
+++ report.php
@@ -1,6 +1,47 @@
-<?php
+<!-- <?php
 echo "Report";
+?> -->
+
+
+
+<?php
+include("includes/connect_database.php");
+
+
+$logged_user_id= $_GET['logged_user_id'];
+$other_user_id = $_GET['other_id'];
+
+$sql = "INSERT INTO reported (user_1, reported_user) VALUES ($logged_user_id, $other_user_id)";
+
+
+
+
+if (mysqli_query($connect,$sql)) {
+
+    echo "<script> alert('⛔ You reported this user!');
+    window.location.href='user_home.php?id={$logged_user_id}';
+    </script>
+    ";
+
+} else {
+  echo "Error: " . $sql . "<br>" . $connect->error;
+}
+
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
