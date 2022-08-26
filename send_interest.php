e06d873aa370f77ba4e118ee5ac5e0c3f237f021
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2022-08-26 21:22:08 +0600

    Send interest implementation is complete

--- send_interest.php
+++ send_interest.php
@@ -6,12 +6,24 @@ echo "Send interest";
 include("includes/connect_database.php");
 
 
+$logged_user_id= $_GET['logged_user_id'];
+$other_user_id = $_GET['other_id'];
+
+$sql = "INSERT INTO send_interest (user_1, interest_on_user) VALUES ($logged_user_id, $other_user_id)";
+
+$results = $connect -> query($sql);
 
 
+if (mysqli_query($connect,$sql)) {
 
+    echo "<script> alert('💝 Interest is sent!');
+    window.location.href='visit_other.php?other_id={$other_user_id}&logged_user_id={$logged_user_id}';
+    </script>
+    ";
 
-
-
+} else {
+  echo "Error: " . $sql . "<br>" . $connect->error;
+}
 
 ?>
 
