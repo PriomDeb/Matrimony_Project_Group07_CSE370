9d93197f95049ab757521fea5bc48c4a05cd9559
PriomDeb <87948186+PriomDeb@users.noreply.github.com>
2022-08-26 12:17:51 +0600

    Update user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -329,12 +329,12 @@ $(document).ready(function(){
 				        	<tbody>
 				        	
 							    <tr class="opened">
-									<td class="day_label">Mother's Name and Occupation:</td>
-									<td class="day_value closed"><span><?php echo $father_info;?></span></td>
+									<td class="day_label">Mother's Name and Occupation: </td>
+									<td class="day_value closed"><span><?php echo $mother_info; ?></span></td>
 								</tr>
 							    <tr class="opened">
-									<td class="day_label">Father's Name and Occupation:</td>
-									<td class="day_value closed"><span><?php echo $mother_info;?></span></td>
+									<td class="day_label">Father's Name and Occupation: </td>
+									<td class="day_value closed"><span><?php echo $father_info; ?></span></td>
 								</tr>
 
 
